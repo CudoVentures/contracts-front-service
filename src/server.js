bedb68a673fb6c8c4abcff974ada44726ecbc2df
avalkov <avalkov@mail.bg>
2022-08-03 14:19:02 +0300

    Added source code size limit

--- src/server.js
+++ src/server.js
@@ -22,10 +22,12 @@ app.listen(LISTEN_PORT, () => {
 
 connectDB('contracts_scan', 'sources', 'schemas', 'verification_results', 'parsing_results').then((dbConn) => {
 
-    app.post('/verify-contract', multer().single('source'), getVerifyContractHandler(dbConn));
+    const multerOptions = { limits: { fileSize: parseInt(process.env.MAX_SOURCE_CODE_SIZE_LIMIT, 10) }};
+
+    app.post('/verify-contract', multer(multerOptions).single('source'), getVerifyContractHandler(dbConn));
     app.get('/verification-status', getVerificationStatusHandler(dbConn));
 
-    // app.post('/parse-contract', multer().single('source'), getParseContractHandler(dbConn));
+    // app.post('/parse-contract', multer(multerOptions).single('source'), getParseContractHandler(dbConn));
     // app.get('/parsing-status', getParsingStatusHandler(dbConn));
 
     app.get('/contract-schemas', getListContractSchemasHandler(dbConn));
